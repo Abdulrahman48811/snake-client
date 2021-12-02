df896226d80c3af8117207bb6a7acbea73cae276
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-12-02 19:46:57 +0000

    add say

--- input.js
+++ input.js
@@ -21,7 +21,13 @@ const handleUserInput = function(data) {
   if (data === 'a') connection.write('Move: left');
   if (data === 'd') connection.write('Move: right');
   if (data === 's') connection.write('Move: down');
+
+  if (data === '1') connection.write('Say: Catch me Haha');
+  if (data === '2') connection.write('Say: I want to speed up');
+  if (data === '3') connection.write('Say: HAHAHA');
+  if (data === '4') connection.write('Say: GO Faster');
 };
+
 
 
 module.exports = { setupInput };
