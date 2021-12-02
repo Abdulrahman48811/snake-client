4ec5cb59698b6f9c8fb6ebc5a4d4ecab9eabd144
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-12-02 19:58:46 +0000

    implemeting constants & speed

---
+++ constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+  IP: '165.227.47.243',
+  PORT: 50541,
+  moveKeys: {
+    w: 'Move: up',
+    a: 'Move: left',
+    d: 'Move: right',
+    s: 'Move: down',
+  },
+  msgKeys: {
+    '1': 'Say: Catch me Haha',
+      '2': 'Say: I want to speed up',
+    '3': 'Say: HAHAHA',
+      '4': 'Say: GO Faster'
+  }
+};
