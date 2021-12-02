4ae9852091bf7cfe5c71c08476ad474038f7225d
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-12-02 01:31:43 +0000

    snake connect & write name

--- client.js
+++ client.js
@@ -1,33 +1,26 @@
 const net = require("net");
-// const {IP, PORT} = require('./constants')
 const connect = function () {
-const conn = net.createConnection({
+  const conn = net.createConnection({
     host: '165.227.47.243',
     port: '50541'
-});
-  return conn;
-};
-console.log("Connecting ...");
-const conn = connect();
-
-  // interpret incoming data as text
+  });
+  
+  conn.setEncoding("utf8");
+ 
   conn.on('connect', () => {
     console.log("Successfully connected to the the server")
 
   });
     
   conn.on('connect', () => {
-    conn.write("Name : Abdulrahman");
+    conn.write("Name: AIM");
   });
 
   conn.on('data', (data) => {
     console.log(data);
   });
 
-
-
-  conn.setEncoding("utf8");
-// return conn;
-
+  return conn;
+};
 
 module.exports = { connect };
